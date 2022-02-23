253e256fc38a20f156ed7e64127796f44fd64699
beansBin <beans_bin@naver.com>
2022-02-23 14:09:25 +0900

    [SETTING] AppDelegate didFinishLaunchingWIthOptions iOS 13.0 분기처리 삭제

--- FakeBithumbAssignment/FakeBithumbAssignment/Global/Supports/AppDelegate.swift
+++ FakeBithumbAssignment/FakeBithumbAssignment/Global/Supports/AppDelegate.swift
@@ -13,13 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if #available(iOS 13.0, *) {
-            return true
-        }
-        
-        window = UIWindow()
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
         return true
     }
     
