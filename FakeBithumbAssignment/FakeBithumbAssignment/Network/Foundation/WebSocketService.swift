e0869d9765fa5b5fe5bfb8e7ba65eb2e035db034
momo-youngg <yeongmoj94+dev@gmail.com>
2022-02-24 16:57:06 +0900

    [FEAT] WebSocketService에서 실제 요청부분 구현 #7
    - web socket으로 요청을 보낸 후
    - 응답으로 온 json에 대해 decodable 타입으로 변환 해준 후
    - 이를 파라미터로 받는 handler closure를 호출 해주는 메소드 구현.
    - 응답에 대해선, text와 error만 핸들링 하도록 되어 있음.
    - 필요 시 나머지 응답도 핸들링 해주도록 추가하면 됨.
    - 공통 에러처리부 아직 추가 전이므로 TODO로 남겨놓았음.

--- FakeBithumbAssignment/FakeBithumbAssignment/Network/Foundation/WebSocketService.swift
+++ FakeBithumbAssignment/FakeBithumbAssignment/Network/Foundation/WebSocketService.swift
@@ -12,11 +12,46 @@ import Starscream
 class WebSocketService {
     let timeOutInterval: TimeInterval = 5
     
-    func request(to url: URL, responseHandler: WebSocketReceiveDelegate) {
+    func subscribe<T: Decodable>(to url: URL,
+                                 writeWith filter: String?,
+                                 _ responseHandler: @escaping (T, WebSocketWrapper?) -> Void) {
         var request = URLRequest(url: url)
         request.timeoutInterval = timeOutInterval
         let socket = WebSocket(request: request)
-        socket.delegate = responseHandler
+        socket.setOnEvent(with: responseHandler)
         socket.connect()
+    }
+}
+
+extension WebSocket {
+    func setOnEvent<T:Decodable>(with responseHandler: @escaping (T, WebSocketWrapper?) -> Void) {
+        self.onEvent = { [weak self] event in
+            switch event {
+            case .text(let jsonString):
+                self?.handleStringResponse(of: jsonString, with: responseHandler)
+            case .error(let error):
+                self?.handleErrorResponse(of: error)
+            default:
+                break
+            }
+        }
+    }
+    
+    private func handleStringResponse<T:Decodable>(of stringResponse: String,
+                                                   with responseHandler: (T, WebSocketWrapper?) -> Void) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        
+        guard let decodedResponse = jsonDecoder.decode(T.self, from: stringResponse) else {
+            return
+        }
+        responseHandler(decodedResponse, WebSocketWrapper(of: self))
+    }
+    
+    private func handleErrorResponse(of error: Error?) {
+        // TODO: 에러 처리
+        print(error!)
     }
 }
