8526860e8e0585396fcd3bb41fa3bbe15a54dc69
momo-youngg <yeongmoj94+dev@gmail.com>
2022-02-24 16:55:29 +0900

    [FEAT] WebSocketApiServiceable 추가 #7
    - controller에서 빗썸 web socket api로의 요청을 보내게 될 타입의
    - 메소드 선언부를 프로토콜로 작성.

--- FakeBithumbAssignment/FakeBithumbAssignment/Network/Foundation/WebSocketApiServiceable.swift
+++ FakeBithumbAssignment/FakeBithumbAssignment/Network/Foundation/WebSocketApiServiceable.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+/// 빗썸 web socket api의 선언부가 있는 프로토콜
+protocol WebSocketApiServiceable {
+    /// 현재가 ticker
+    ///
+    /// - Parameter symbols 통화코드
+    /// - Parameter tickTypes: tick 종류
+    /// - Parameter responseHander: web socket response와 disconnnect할 WebSocketWrapper 파라미터 타입의 클로져
+    func connectTicker(symbols: [WebSocketApiRequest.Symbol],
+                       tickTypes: [WebSocketApiRequest.TickType]?,
+                       responseHandler: (WebSocketApiResponse.TickerResponse, WebSocketWrapper) -> Void)
+    
+    /// 체결 transaction
+    ///
+    /// - Parameter symbols: 통화코드
+    /// - Parameter responseHander: web socket response와 disconnnect할 WebSocketWrapper 파라미터 타입의 클로져
+    func connectTransaction(symbols: [WebSocketApiRequest.Symbol],
+                            responseHandler: (WebSocketApiResponse.TransactionResponse, WebSocketWrapper) -> Void)
+
+    /// 호가 orderbook
+    ///
+    /// - Parameter symbols: 통화코드
+    /// - Parameter responseHander: web socket response와 disconnnect할 WebSocketWrapper 파라미터 타입의 클로져
+    func connectOrderBook(symbols: [WebSocketApiRequest.Symbol],
+                            responseHandler: (WebSocketApiResponse.OrderBookResponse, WebSocketWrapper) -> Void)
+}
