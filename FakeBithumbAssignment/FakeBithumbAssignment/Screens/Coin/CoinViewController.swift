81e4619c5d1614f7614073cf33a7e2fb461c56b9
beansBin <beans_bin@naver.com>
2022-02-26 10:10:06 +0900

    [FEAT] : 상단 정보창 UI 및 오토레이아웃 (#9)

--- FakeBithumbAssignment/FakeBithumbAssignment/Screens/Coin/CoinViewController.swift
+++ FakeBithumbAssignment/FakeBithumbAssignment/Screens/Coin/CoinViewController.swift
@@ -7,23 +7,87 @@
 
 import UIKit
 
-class CoinViewController: UIViewController {
+import SnapKit
+import Then
+
+final class CoinViewController: BaseViewController {
+    
+    // MARK: - Instance Property
+    
+    private let currentPriceLabel = UILabel().then {
+        $0.text = "45,594,000"
+        $0.font = UIFont.systemFont(ofSize: 25)
+    }
+    
+    private let fluctateLabel = UILabel().then {
+        $0.text = "-1,578,000"
+        $0.font = UIFont.systemFont(ofSize: 15)
+    }
+    
+    private let fluctateImageView = UIImageView().then {
+        $0.image = UIImage()
+    }
+    
+    private let fluctateRateLabel = UILabel().then {
+        $0.text = "3.35%"
+        $0.font = UIFont.systemFont(ofSize: 15)
+    }
+    
+    private let informationView = UIView()
+    private let pageView = UIView()
+    
+    private let menuCollectionView: UICollectionView = {
+        var collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout.init())
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: UICollectionViewCell.className)
+        return collectionView
+    }()
+    
+    
+    // MARK: - Life Cycle func
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        render()
+        configUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func render() {
+        view.addSubViews([currentPriceLabel, fluctateLabel, fluctateImageView, fluctateRateLabel, pageView])
+        
+        currentPriceLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+            make.leading.equalTo(view).offset(20)
+            make.width.equalTo(150)
+        }
+        
+        fluctateLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(currentPriceLabel.snp.bottom)
+            make.leading.equalTo(view).offset(20)
+        }
+        
+        fluctateImageView.snp.makeConstraints { (make) in
+            make.top.equalTo(currentPriceLabel.snp.bottom)
+            make.leading.equalTo(fluctateLabel.snp.trailing).offset(10)
+            make.width.height.equalTo(18)
+        }
+        
+        fluctateRateLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(currentPriceLabel.snp.bottom)
+            make.leading.equalTo(fluctateImageView.snp.trailing)
+        }
     }
-    */
+    
+    override func configUI() {
+        super.configUI()
+    }
+        
+    
+    
+    // MARK: - @IBOutlets Action
+    
+    // MARK: - custom func
+    
+    // MARK: - @objc
 
 }
