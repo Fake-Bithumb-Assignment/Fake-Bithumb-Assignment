5bf12c67dec6d1c76bc7344ac1bfaabc3e711436
momo <yeongmoj94+dev@gmail.com>
2022-02-26 21:10:14 +0900

    [FEAT] : 차트에 필요한 옵션값들 프로퍼티로 추가 (#19)

--- FakeBithumbAssignment/FakeBithumbAssignment/Screens/Main/View/CandleStickChartView.swift
+++ FakeBithumbAssignment/FakeBithumbAssignment/Screens/Main/View/CandleStickChartView.swift
@@ -13,15 +13,33 @@ class CandleStickChartView: UIView {
     
     // MARK: - instance property
     
+    /// 스크롤 뷰
     private let scrollView: UIScrollView = UIScrollView().then {
         $0.backgroundColor = .clear
-    } // 스크롤 뷰
-    private let mainLayer: CALayer = CALayer() // 스크롤 뷰를 가득 채울 레이어
-    private let dataLayer: CALayer = CALayer() // dateTimeLayer를 제외하고 실제 차트가 그려질 레이어 (스크롤에 포함)
-    private let dateTimeLayer: CALayer = CALayer() // 아래 날짜 혹은 시간 영역을 그려 줄 레이어 (스크롤에 포함)
-    private let valueLayer: CALayer = CALayer() // 오른쪽 값 영역을 그려줄 레이어
-    private let horizontalGridLayer: CALayer = CALayer() // 가로줄을 그려줄 레이어
-    private let verticalGridLayer: CALayer = CALayer() // 세로줄을 그려줄 레이어 (스크롤에 포함)
+    }
+    /// 스크롤 뷰를 가득 채울 레이어
+    private let mainLayer: CALayer = CALayer()
+    /// dateTimeLayer를 제외하고 실제 차트가 그려질 레이어 (스크롤에 포함)
+    private let dataLayer: CALayer = CALayer()
+    /// 아래 날짜, 시간 영역을 그려 줄 레이어 (스크롤에 포함)
+    private let dateTimeLayer: CALayer = CALayer()
+    /// 오른쪽 값 영역을 그려줄 레이어
+    private let valueLayer: CALayer = CALayer()
+    /// 가로줄을 그려줄 레이어
+    private let horizontalGridLayer: CALayer = CALayer()
+    /// 세로줄을 그려줄 레이어 (스크롤에 포함)
+    private let verticalGridLayer: CALayer = CALayer()
+    
+    /// 오른쪽 값 영역의 너비
+    private var valueWidth: CGFloat = 40.0
+    /// 아래 날짜, 시간 영역의 높이
+    private var dateTimeHeight: CGFloat = 20.0
+    /// 캔들스틱 너비
+    private var candleStickWidth: CGFloat = 5.0
+    /// 캔들스틱 간격
+    private var candleStickSpace: CGFloat = 1.0
+    /// 그래프 맨앞, 맨 뒤의 빈 공간
+    private var widthFrontRearSpace: CGFloat = 40.0
     
     // MARK: - Initializer
     
@@ -53,11 +71,9 @@ class CandleStickChartView: UIView {
         self.layer.addSublayer(valueLayer)
         // subView로 추가
         self.addSubview(scrollView)
-        self.backgroundColor = .clear
     }
     
     override func layoutSubviews() {
         // Frame 설정
     }
-    
 }
