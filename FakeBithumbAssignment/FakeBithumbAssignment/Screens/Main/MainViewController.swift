d191bb67a882432aac326085971e1b48aac6ba83
kch1285 <kch1285@naver.com>
2022-02-27 23:18:32 +0900

    [FEAT] : 테이블뷰 셀과 테이블뷰 연결하여 레이아웃 구현 (#17)

--- FakeBithumbAssignment/FakeBithumbAssignment/Screens/Main/MainViewController.swift
+++ FakeBithumbAssignment/FakeBithumbAssignment/Screens/Main/MainViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import SnapKit
+import Then
 
 final class MainViewController: BaseViewController {
 
@@ -15,14 +16,56 @@ final class MainViewController: BaseViewController {
 
     private let headerView = HeaderView()
 
+    private let coinTableView = UITableView().then {
+        $0.register(CoinTableViewCell.self, forCellReuseIdentifier: CoinTableViewCell.className)
+    }
+
     // MARK: - custom func
 
     override func render() {
+        configureHeaderView()
+        configureTableView()
+    }
+
+    private func configureHeaderView() {
         view.addSubview(headerView)
         headerView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.height.lessThanOrEqualTo(200)
         }
+    }
+
+    private func configureTableView() {
+        coinTableView.dataSource = self
+
+        view.addSubview(coinTableView)
+        guard let tabBarHeight = self.tabBarController?.tabBar.frame.size.height
+        else {
+            return
+        }
+
+        coinTableView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(tabBarHeight)
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: CoinTableViewCell.className,
+            for: indexPath
+        )
+
+        return cell
     }
 }
