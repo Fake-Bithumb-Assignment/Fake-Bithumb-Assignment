da4ae07d24156cfa6c0b701df5525b350cd9212e
beansBin <beans_bin@naver.com>
2022-03-01 19:13:18 +0900

    [FEAT] : 호가정보창 scrollView 생성 및 테스트라벨 1, 2 생성 (#29)

--- FakeBithumbAssignment/FakeBithumbAssignment/Screens/Coin/Tabs/CoinQuoteInformationTab/CoinQuoteInformationTabViewController.swift
+++ FakeBithumbAssignment/FakeBithumbAssignment/Screens/Coin/Tabs/CoinQuoteInformationTab/CoinQuoteInformationTabViewController.swift
@@ -7,23 +7,58 @@
 
 import UIKit
 
-class CoinQuoteInformationTabViewController: UIViewController {
+import SnapKit
+import Then
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class CoinQuoteInformationTabViewController: BaseViewController {
+    
+    // MARK: - Instance Property
+    
+    let labelOne = UILabel().then { make in
+        make.text = "Scroll Top"
+        make.backgroundColor = .red
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    let labelTwo = UILabel().then { make in
+        make.text = "Scroll Top"
+        make.backgroundColor = .green
     }
-    */
-
+    
+    let scrollView = UIScrollView().then { make in
+        make.backgroundColor = .cyan
+    }
+    
+    // MARK: - Life Cycle func
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        render()
+    }
+    
+    override func render() {
+        self.view.addSubview(self.scrollView)
+        
+        self.scrollView.snp.makeConstraints { make in
+            make.left.top.right.bottom.equalTo(self.view)
+        }
+        
+        self.scrollView.addSubview(self.labelOne)
+        
+        self.labelOne.snp.makeConstraints { make in
+            make.leading.top.equalTo(self.scrollView).offset(16)
+            
+        }
+        
+        self.scrollView.addSubview(self.labelTwo)
+        
+        self.labelTwo.snp.makeConstraints { make in
+            make.leading.equalTo(self.scrollView).offset(200)
+            make.top.equalTo(self.scrollView).offset(1000)
+            make.trailing.bottom.equalTo(self.scrollView).inset(16)
+        }
+    }
+    
+    override func configUI() {
+        
+    }
 }
