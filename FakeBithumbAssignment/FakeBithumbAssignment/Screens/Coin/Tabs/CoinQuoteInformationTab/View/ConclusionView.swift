78ffbc93bcf1576db4198f4a48b13877cfd1a5b4
beansBin <beans_bin@naver.com>
2022-03-02 10:14:26 +0900

    [FEAT] : QuoteTabView 세부 뷰 6개 생성 (#29)

---
+++ FakeBithumbAssignment/FakeBithumbAssignment/Screens/Coin/Tabs/CoinQuoteInformationTab/View/ConclusionView.swift
@@ -0,0 +1,31 @@
+//
+//  ConcolusionGraphView.swift
+//  FakeBithumbAssignment
+//
+//  Created by 박예빈 on 2022/03/02.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ConclusionView: UIView {
+    
+    // MARK: - Life Cycle func
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configUI() {
+        self.backgroundColor = .red
+    }
+    
+}
---
+++ FakeBithumbAssignment/FakeBithumbAssignment/Screens/Coin/Tabs/CoinQuoteInformationTab/View/QuoteView.swift
@@ -0,0 +1,32 @@
+//
+//  SellBuyView.swift
+//  FakeBithumbAssignment
+//
+//  Created by 박예빈 on 2022/03/02.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class QuoteView: UIView {
+    
+    // MARK: - Life Cycle func
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configUI() {
+        self.backgroundColor = .yellow
+    }
+    
+}
+
