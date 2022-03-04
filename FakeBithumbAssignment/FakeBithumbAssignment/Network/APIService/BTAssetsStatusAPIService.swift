e346878518c9b08436fcaf5c6233182ddde020b0
momo <yeongmoj94+dev@gmail.com>
2022-03-05 06:10:44 +0900

    [FEAT] : 입출금 내역 API 추가 (#42)

---
+++ FakeBithumbAssignment/FakeBithumbAssignment/Network/APIService/BTAssetsStatusAPIService.swift
@@ -0,0 +1,33 @@
+//
+//  BTAssetsStatusAPIService.swift
+//  FakeBithumbAssignment
+//
+//  Created by momo on 2022/03/05.
+//
+
+import Foundation
+
+struct BTAssetsStatusAPIService {
+    private let httpService: HttpService = HttpService()
+    
+    func requestAssetsStatus(
+        of orderCurrency: Coin
+    ) async -> [BTAssetsStatusResponse] {
+        let url: String = "https://api.bithumb.com/public/assetsstatus/\(String(describing: orderCurrency))"
+        let request: NetworkRequest = NetworkRequest(
+            url: url,
+            headers: nil,
+            reqBody: nil,
+            reqTimeout: nil,
+            httpMethod: .GET
+        )
+        do {
+            guard let response: [BTAssetsStatusResponse] = try await httpService.request(request) else {
+                return []
+            }
+            return response
+        } catch {
+            return []
+        }
+    }
+}
