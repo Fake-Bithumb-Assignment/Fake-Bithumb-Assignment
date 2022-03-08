11c8f40699858ab0dadca9e16168e25c477014c2
kch1285 <kch1285@naver.com>
2022-03-08 14:12:35 +0900

    [FEAT] : 탭바 생성 및 이미지 적용 (#47)

---
+++ FakeBithumbAssignment/FakeBithumbAssignment/Screens/Main/MainTabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarViewController.swift
+//  FakeBithumbAssignment
+//
+//  Created by chihoooon on 2022/03/08.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
