1e61dca2b2bd41fe07e506c68886819f1c5bb1f6
beansBin <beans_bin@naver.com>
2022-03-10 01:42:23 +0900

    [FEAT] : splash view 2초 시간 지연 추가 (#63)

--- FakeBithumbAssignment/FakeBithumbAssignment/Global/Supports/AppDelegate.swift
+++ FakeBithumbAssignment/FakeBithumbAssignment/Global/Supports/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 2.0)
         return true
     }
     
